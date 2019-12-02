eb86ef14c50fac091b5606e94c6d5b678326597d
Net-Realite <alexcle@yahoo.fr>
2019-12-02 14:53:18 +0100

    init id commit

---
+++ src/Board.java
@@ -0,0 +1,9 @@
+public class Board {
+
+    Coordinate coordinate = new Coordinate();
+
+    <coordinate> squareStatus(coordinate){
+
+    }
+
+}
---
+++ src/Ship.java
@@ -0,0 +1,12 @@
+import javafx.geometry.Orientation;
+
+public class Ship {
+    int size;
+    Coordinate coordinate;
+    Orientation orientation;
+
+    boolean isOnSquare(coordinate){
+
+    }
+
+}
